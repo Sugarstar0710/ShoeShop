d0db59894b4aab9a70126d0da4fd697888ecac84
hoang <hoangchuong07102003@gmail.com>
2025-07-11 12:52:07 +0700

    ShoeShop

---
+++ ShoeShop/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ShoeShop from './ShoeShop'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <ShoeShop/>
+    </>
+  )
+}
+
+export default App
---
+++ ShoeShop/src/ShoeShop/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ShoeShop() {
+  return (
+    <div>ShoeShop</div>
+  )
+}
